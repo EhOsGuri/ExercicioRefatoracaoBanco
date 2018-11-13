d676218e91f222721c1a67e607c97202abdfc71f
GABRIEL VAZ DE SOUZA <17111238@pucrs.br>
2018-11-13 10:42:16 -0200

    accounts class

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Contas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Contas.java
@@ -5,10 +5,37 @@
  */
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author 17111238
  */
 public class Contas {
+
+    private static Contas uniqueInstance;
+    private  Map<Integer,Conta> contas;
+    
+    private Contas() {
+        contas = Persistencia.getInstance().loadContas();
+    }
+    
+    public static Contas getInstance() {
+        if (uniqueInstance == null)
+            uniqueInstance = new Contas();
+ 
+        return uniqueInstance;
+    }
+    
+    public void saveContas(){
+        Persistencia.getInstance().saveContas(contas.values());
+    }
+    
+    public Map<Integer,Conta> getContas() {
+        return contas;
+    }
+    
+  
     
 }
